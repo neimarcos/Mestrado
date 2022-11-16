0bc6463946a059e82e31f5d54c10b55745515921
Neimar <neimar.assmann@gmail.com>
2022-11-16 00:13:56 -0300

    Atualizações

--- principal.py
+++ principal.py
@@ -4,13 +4,12 @@ import networkx as nx
 import pandas as pd
 
 
-
 def ClearRoutes(spf):
     """
-    Clear routes, remove reverse path.
-    ### Parameters
-    1. spf : dict
-        - [dictionary of dictionaries with path[source][target]=[list of nodes in path], return from function shortest_path from networkx library]
+    Clear routes, remove reverse path. We will only use it with bidirectional measurement
+
+    ### Parameters    
+    spf (dict) : dictionary of dictionaries with path[source][target]=[list of nodes in path], return from function shortest_path from networkx library
 
     ### Returns
         dict : routes with only one path between two host 
@@ -30,13 +29,14 @@ def ClearRoutes(spf):
 def Count_Subsegment_Occurrences(routes):
     """
     Find and count how many times each subsegment occurs.
-    ### Parameters
-    1. spf : dict
-        - [routes]
+    
+    ### Parameters:
+    spf (dict) : routes with only one path between two host 
 
     ### Returns
         dict : with the subsegments and how many times they occur in the path
     """
+    
     ### Clear routes, remove reverse path 
     routes_aux = routes.copy()
     index = 0
@@ -55,17 +55,40 @@ def Count_Subsegment_Occurrences(routes):
         Path_Count_Occurrences.append((route, count, len(route)))
     return (Path_Count_Occurrences)
 
-def encontraposicoes(pos, rota, subrotas):
+def encontraposicoes(pos, route, subrotas):
+    """
+    Finds  
+
+    ### Parameters:
+        pos (int): current position on the route to start checking
+        route (list): _description_
+        subrotas (list): _description_
+
+    ### Returns:
+        list: _description_
+    """    
     posicoes = []
-    if rota[pos] != rota[len(rota)-1]:
+    if route[pos] != route[len(route)-1]:
         for subrota in range(len(subrotas)):
-            if rota[pos] == subrotas[subrota][0]:
+            if route[pos] == subrotas[subrota][0]:
                 posicoes.append(subrota)
     return (posicoes)
 
 
 
-def compoe_subrotas(pos, par_rota_composta, rota, subrotas, pesos):
+def compoe_subrotas(par_rota_composta, rota, subrotas, pos = 0):
+    """
+    Finds all possible subpath compositions of a path    
+    
+    ### Parameters:
+        par_rota_composta (list): _description_
+        rota (list): path to finds possible compositions
+        subrotas (list): all possibles subpaths 
+        pos (int): initial position to find 
+        
+    ### Returns:
+        list: List of all possible subpath compositions of a path
+    """    
     posicoes = encontraposicoes(pos, rota, subrotas)
     saida = []
     if posicoes == []:
@@ -74,70 +97,76 @@ def compoe_subrotas(pos, par_rota_composta, rota, subrotas, pesos):
         return (saida)
     else:
         rota_composta = []
-    peso_total = 0
     for posicao in posicoes:
         rota_composta = copy.deepcopy(par_rota_composta)
         rota_composta.append(subrotas[posicao])
-        saltos = compoe_subrotas(rota.index(subrotas[posicao][(
-            len(subrotas[posicao])-1)]), rota_composta, rota, subrotas, pesos)
+        saltos = compoe_subrotas( rota_composta, rota, subrotas, rota.index(subrotas[posicao][(len(subrotas[posicao])-1)]))
         saida.extend(saltos)
     return (saida)
 
 def Find_Compose_Paths(path_count):
-    pesos=tuple([k[1] for k in path_count])
-    caminhos=tuple([k[0] for k in path_count])
+    """ 
+    Finds all possible compositions for all paths
+    
+    ### Parameters:
+        path_count (list): all paths/subpaths and how many times it repeats
+    ### Returns:
+        list: all possible possible combinations of subpaths for all paths
+    """
+    paths=tuple([k[0] for k in path_count])
     rotascompostas = []
     global Lista_Medicoes
     global Lista_Sonda_Medicao
-    for i in spf:
-        for k, v in spf[i].items():          
-            if len(v) > 2:
-                #pprint(f"Origem: {i}, destino: {k}, rota spf: {v} ")
-                test_str = v
-                subcaminhos = []
-                pesos_subcaminhos = []
-                # rotascompostas.append(v)
-                for i in range(len(test_str)):
-                    for j in range(i + 1, len(test_str) + 1):
-                        subcaminho = (test_str[i: j])
-                        subcaminho_reverse = list(reversed(subcaminho))
-                        if len(subcaminho) > 1 and len(subcaminho) < len(test_str):
-                        # pprint(subcaminho)
-                        # pprint(subcaminho_reverse)
-                            try:
-                                if subcaminho in caminhos:
-                                    pos = caminhos.index(subcaminho)
-                                    #pprint(f"Subcaminho {subcaminho} na posição {pos} com peso {pesos[pos]}  " )
-                                    subcaminhos.append(subcaminho)
-                                    pesos_subcaminhos.append(pesos[pos])
-                                else:
-                                    pos = caminhos.index(subcaminho_reverse)
-                                    #pprint(f"Subcaminho_reverse {subcaminho_reverse} na posição {pos} com peso {pesos[pos]}  " )
-                                    subcaminhos.append(subcaminho)
-                                    pesos_subcaminhos.append(pesos[pos])
-                            except ValueError as e:
-                                pprint("Subcaminho não existente")
-                caminhos_par = []
-                #print(f'SubRotas: {subcaminhos}')
-                # pprint(compoe_subrotas(0,caminhos_par))
-                retorno = (compoe_subrotas(0, caminhos_par, v, subcaminhos, pesos_subcaminhos))
-                rotascompostas.append (retorno)
-                for comp in retorno:
-                    Lista_Medicoes.append(v)
-                    Lista_Sonda_Medicao.append(comp)
-            elif len(v) > 1:
-                Lista_Medicoes.append(v)
-                Lista_Sonda_Medicao.append(v)
-    # for i, nome in enumerate(neimar):
-    #     pprint(f'IDX {i} - rota {nome}')
-    #     for compos in rotascompostas[i]:
-    #         if type(compos) is str:
-    #             pprint(rotascompostas[i])
-    #             break
-    #         else:
-    #             pprint (compos)
+    for path in paths:
+        if len(path) > 2:
+            #pprint(f"Origem: {i}, destino: {k}, rota spf: {v} ")
+            subpaths = []
+            for i in range(len(path)):
+                for j in range(i + 1, len(path) + 1):
+                    subpath = (path[i: j])
+                    if (len(subpath) > 1) and (len(subpath) < len(path)):
+                        try:
+                            subpaths.append(subpath)
+                        except ValueError as e:
+                            pprint("Subcaminho não existente")
+            caminhos_par = []
+            retorno = (compoe_subrotas(caminhos_par, path, subpaths))
+            rotascompostas.append (retorno)
+            for comp in retorno:
+                Lista_Medicoes.append(path)
+                Lista_Sonda_Medicao.append(comp)
+        elif len(path) > 1:
+            Lista_Medicoes.append(path)
+            Lista_Sonda_Medicao.append(path)
     return (rotascompostas)
 
+def Composite_Route_Cost(df, rotascompostas):
+    """
+    Calculate cost based composition of subpaths
+    
+    ### Parameters:
+        df (dataframe): dataframe with all paths
+        rotascompostas (list): all possible possible compositon of subpaths for all paths
+        
+        
+    """
+    def encontrapeso(salto):
+        df2 = df[df['path_str'].astype(str) == str(salto)]
+        if df2.empty:
+            reverso = list(reversed(salto))
+            df2 = df[df['path_str'].astype(str) == str(reverso)]
+        return (2 * (df2.iloc[0]['count']))
+    global Lista_Sonda_Medicao_Peso
+    
+    for rotascomp in rotascompostas:
+        val = 0
+        for composicao in rotascomp:
+            if type(composicao) is str:
+                val = encontrapeso(rotascomp)
+                break
+            else:
+                val += encontrapeso(composicao)
+        Lista_Sonda_Medicao_Peso.append(val)
 
 
 #rede = 'Geant2012.graphml'
@@ -166,4 +195,8 @@ Lista_Sonda_Medicao_Peso = []
 
 compose_paths = Find_Compose_Paths(path_count)
 
-pprint(compose_paths)
+Composite_Route_Cost(df, Lista_Sonda_Medicao)
+
+
+
+pprint(Lista_Sonda_Medicao_Peso)
