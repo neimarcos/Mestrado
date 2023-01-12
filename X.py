40ae0478ca8c2a53327e6003efbbcb49d274c240
Neimar <neimar.assmann@gmail.com>
2023-01-12 02:54:35 -0300

    Antes do plot

--- X.py
+++ X.py
@@ -4,7 +4,7 @@ import networkx as nx
 import numpy as np
 from pulp import *
 import time
-
+import concurrent.futures
 
 def ClearRoutes(spf):
     """
@@ -207,199 +207,218 @@ def End(NameFunction):
     pprint(f'Concluiu a função {NameFunction}, em {time.process_time() - inicio:.2f} segundos')
     pprint(f'#################################################################################')
 
-#rede = 'Geant2012.graphml'
-#rede = 'Rnp.graphml'
-#rede = 'exemplo.graphml'
-rede = 'exemplo_pequeno.graphml'
 
-G = nx.read_graphml(rede)
-spf = nx.shortest_path(G, weight='LinkSpeedRaw')
-
-# Clear routes
-
-inicio=time.process_time()
-
-Start('ClearRoutes')
-paths = ClearRoutes (spf)
-End('ClearRoutes')
-
-Start('Count_Subsegment_Occurrences')
-# counts how many times a subsegment/subpath occurs in the spf 
-path_count, path_cost = Count_Subsegment_Occurrences(paths)
-End('Count_Subsegment_Occurrences')
-
-Measurements_List = []
-Probes_List = []
-Cost_List = []
-
-Start('Find_Compose_Paths')
-compose_paths = Find_Compose_Paths(path_count)
-End('Find_Compose_Paths')
-
-Start('Compose_Route_Cost')
-Compose_Route_Cost(Probes_List)
-End('Compose_Route_Cost')
-
-#pprint(Probes_List)
-
-max_len_probe = 0
-max_probes_measurement = 0
-for id_probe, probe in enumerate(Probes_List):
-    if Measurements_List[id_probe] != probe:
-        if len(probe) > max_len_probe:
-            max_len_probe = len(probe)
-
-#pprint(max_len_probe)
-
-lista_medicao, num_sonda_medicao = np.unique(Measurements_List, return_counts=True)
-
-
-routers = G.nodes
-
-
-total_roteadores = len(routers)
-
-# Router Source
-RS = total_roteadores
-# Router Destination
-RD = total_roteadores
-# Medições de um roteador para todos os outros
-M = max(num_sonda_medicao)
-# Possiveis composições das medições de um roteador para todos os outos
-C = max_len_probe
-
-SDMC_Cost= np.zeros((RS, RD, M, C))
-SDMC_Id_Medicao = np.zeros((RS, RD, M, C))
-SDMC_Id_Medicao[:][:][:][:] = -1
-nodes_list = np.array(list(G.nodes()))
-
-pprint(SDMC_Id_Medicao.shape)
-Start('Matriz de Custos')
-
-
-medicao_anterior = ''
-id_M = 0
-for idMedicao, Medicao in enumerate(Measurements_List):
-    #if (Probes_List[idMedicao] == Measurements_List [idMedicao]):
-    if str(medicao_anterior)!=str(Medicao):
-        id_RS = np.where(nodes_list == Measurements_List[idMedicao][0])
-        id_RD = np.where(nodes_list == Measurements_List[idMedicao][len(Measurements_List[idMedicao])-1])
-        id_M = 0
-        SDMC_Cost[id_RS,id_RD,id_M,0]=Cost_List[idMedicao]
-        SDMC_Id_Medicao[id_RS,id_RD,id_M,0]=paths.index(Probes_List[idMedicao])
-    else:
-        id_C = 0
-        id_M += 1
-        for probe in Probes_List[idMedicao]:
-            if probe in paths:
-                id_cost = paths.index(probe)
-            else:
-                reverso = list(reversed(probe))
-                id_cost = paths.index(reverso)  
-            SDMC_Cost[id_RS,id_RD,id_M,id_C] = path_cost[id_cost]
-            SDMC_Id_Medicao[id_RS,id_RD,id_M,id_C]=id_cost
-            id_C +=1
-    medicao_anterior = Medicao
-#pprint(SDMC_Cost) 
-End('Matriz de Custos')
-
-    
-
-
-
-
-Start('Inicia o modelo e cria a função de maximização')
-RS_list = [*range(0, total_roteadores,1)]
-RD_list = [*range(0, total_roteadores,1)]
-M_list = [*range(0,max(num_sonda_medicao) ,1)]
-C_list = [*range(0, max_len_probe,1)]
-
-
-SDMC_Dict = LpVariable.dicts("SDMC", (RS_list,RD_list,M_list,C_list), 0, 1, LpInteger)   
-
-#pprint(SDMC_Dict)
-
-modelo_colocacao = LpProblem("Probes Placement Model", LpMaximize)
-
-
-
-modelo_colocacao += (lpSum([SDMC_Dict[i_RS][i_RD][0][0] * SDMC_Cost[i_RS][i_RD][0][0] for i_RS in RS_list for i_RD in RD_list for i_M in M_list for i_C in C_list if i_RS != i_RD]),"Total_Cost")
-
-End('Inicia o modelo e cria a função de maximização')
-
-
-
-
-
-Start('Limita sondas por roteador')
-
-max_sondas = {n: (len(list(nx.all_neighbors(G, n))))for n in G.nodes}
-
-#### ATENÇÃO VER ORIGEM DESTINO ######## 1-> 2 = 2 -> 1
-for id_router, router in enumerate(routers):
-    #if id_router < (len(routers)-1):
-    pprint(f'Limite de sondas no roteador: {router} - {max_sondas.get(router)}')
-    #id_router = np.where(nodes_list == router)[0][0]
-    modelo_colocacao += (lpSum([SDMC_Dict[id_router][i_RD][0][0] for i_RD in RD_list if id_router != i_RD]) <=  max_sondas.get(router), "Max_Probes_Router" + str(id_router))
-
-End('Limita sondas por roteador')
-
-Start('Iguala as sondas')
-
-for id_path, path in enumerate(paths):
-    id_RS = int(np.where(nodes_list == path[0])[0])
-    id_RD = int(np.where(nodes_list == path[len(path)-1])[0])
-    for i_RS in RS_list:
-        for i_RD in RD_list:
-            for i_M in M_list:
-                for i_C in C_list:
-                    if (SDMC_Id_Medicao[i_RS,i_RD,i_M,i_C]) > 0 and (i_M > 0) and (i_C > 0):
-                        if id_path == int(SDMC_Id_Medicao[i_RS,i_RD,i_M,i_C]) :
+def IgualaSondas(paths): 
+    global modelo_colocacao   
+    for id_path, path in enumerate(paths):
+        id_RS = int(np.where(nodes_list == path[0])[0])
+        id_RD = int(np.where(nodes_list == path[len(path)-1])[0])
+        for i_RS in RS_list:
+            for i_RD in RD_list:
+                for i_M in M_list:
+                    for i_C in C_list:
+                        #if (SDMC_Id_Medicao[i_RS,i_RD,i_M,i_C]) > 0 and (i_M > 0) and (i_C > 0):
+                        if id_path == int(SDMC_Id_Medicao[i_RS,i_RD,i_M,i_C]) and (i_M > 0) and (i_C > 0) :
                             modelo_colocacao += SDMC_Dict[i_RS][i_RD][i_M][i_C] == SDMC_Dict[id_RS][id_RD][0][0], "Igual_Probes" + str(path) + '_' + str(i_RS) + '_' + str(i_RD) + '_' + str(i_M) + '_' + str(i_C)
                             #pprint(id_path)
                             #pprint(int(SDMC_Id_Medicao[i_RS,i_RD,i_M,i_C]))
                             #pprint(SDMC_Dict[id_RS][id_RD][0][0])
                             #pprint(SDMC_Dict[i_RS][i_RD][i_M][i_C])
-End('Iguala as sondas')
 
 
-#for i_RS in RS_list:
-#    for i_RD in RD_list:
-#        if i_RS != i_RD:
-#            for i_M in M_list:
-#                if SDMC_Cost[id_RS,id_RD,id_M,0] > 0:
-#                    list_comp = []
-#                    for i_C in C_list:
-#                        if SDMC_Id_Medicao[i_RS][i_RD][i_M][i_C] > 0:
-#                            list_comp.append(i_C)
-#                    if len(list_comp) >0:
-#                        modelo_colocacao += (lpSum([SDMC_Dict[i_RS][i_RD][i_M][i_C] for i_C in list_comp]) == len(list_comp), "Sonda" + str(i_RS) + str(i_RD) + str(i_M))
 
-Start('Salva o Modelo')
-modelo_colocacao.writeLP(rede.replace(".graphml", ".LP"))
-End('Salva o Modelo')
+if __name__ == '__main__':
+    
+    #rede = 'Geant2012.graphml'
+    #rede = 'Rnp.graphml'
+    rede = 'exemplo.graphml'
+    #rede = 'exemplo_pequeno.graphml'
+
+    G = nx.read_graphml(rede)
+    spf = nx.shortest_path(G, weight='LinkSpeedRaw')
+
+    # Clear routes
+
+    inicio=time.process_time()
+
+    Start('ClearRoutes')
+    paths = ClearRoutes (spf)
+    End('ClearRoutes')
+
+    Start('Count_Subsegment_Occurrences')
+    # counts how many times a subsegment/subpath occurs in the spf 
+    path_count, path_cost = Count_Subsegment_Occurrences(paths)
+    End('Count_Subsegment_Occurrences')
+
+    Measurements_List = []
+    Probes_List = []
+    Cost_List = []
+
+    Start('Find_Compose_Paths')
+    compose_paths = Find_Compose_Paths(path_count)
+    End('Find_Compose_Paths')
+
+    Start('Compose_Route_Cost')
+    Compose_Route_Cost(Probes_List)
+    End('Compose_Route_Cost')
+
+    #pprint(Probes_List)
+
+    max_len_probe = 0
+    max_probes_measurement = 0
+    for id_probe, probe in enumerate(Probes_List):
+        if Measurements_List[id_probe] != probe:
+            if len(probe) > max_len_probe:
+                max_len_probe = len(probe)
+
+    #pprint(max_len_probe)
+
+    lista_medicao, num_sonda_medicao = np.unique(Measurements_List, return_counts=True)
 
 
-Start('Pulp Solve')
-modelo_colocacao.solve()
-End('Pulp Solve')
+    routers = G.nodes
 
 
-print("Status:", LpStatus[modelo_colocacao.status])
-if modelo_colocacao.status == 1:
-    #pprint(modelo_colocacao)    
-    for v in modelo_colocacao.variables():
-        if v.varValue > 0:
-            print(v.name, "=", v.varValue)
-            x = v.name.split("_")
-            #if (int(x[3]) == 0 ):
-            pprint(int(SDMC_Id_Medicao[int(x[1])][int(x[2])][int(x[3])][int(x[4])]))
-            pprint(Probes_List[int(SDMC_Id_Medicao[int(x[1])][int(x[2])][int(x[3])][int(x[4])])])
-print("Custo = ", value(modelo_colocacao.objective))
+    total_roteadores = len(routers)
+
+    # Router Source
+    RS = total_roteadores
+    # Router Destination
+    RD = total_roteadores
+    # Medições de um roteador para todos os outros
+    M = max(num_sonda_medicao)
+    # Possiveis composições das medições de um roteador para todos os outos
+    C = max_len_probe
+
+    SDMC_Cost= np.zeros((RS, RD, M, C))
+    SDMC_Id_Medicao = np.zeros((RS, RD, M, C))
+    SDMC_Id_Medicao[:][:][:][:] = -1
+    nodes_list = np.array(list(G.nodes()))
+
+    #pprint(SDMC_Id_Medicao.shape)
+    Start('Matriz de Custos')
 
 
-#pprint(SDMC_Id_Medicao)
-#pprint(Probes_List)
+    medicao_anterior = ''
+    id_M = 0
+    
+    #REVISARRRRRRR
+    for idMedicao, Medicao in enumerate(Measurements_List):
+        #if (Probes_List[idMedicao] == Measurements_List [idMedicao]):
+        if str(medicao_anterior)!=str(Medicao):
+            id_RS = np.where(nodes_list == Measurements_List[idMedicao][0])
+            id_RD = np.where(nodes_list == Measurements_List[idMedicao][len(Measurements_List[idMedicao])-1])
+            id_M = 0
+            SDMC_Cost[id_RS,id_RD,id_M,0]=Cost_List[idMedicao]
+            SDMC_Id_Medicao[id_RS,id_RD,id_M,0]=paths.index(Probes_List[idMedicao])
+        else:
+            id_C = 0
+            id_M += 1
+            for probe in Probes_List[idMedicao]:
+                if probe in paths:
+                    id_cost = paths.index(probe)
+                else:
+                    reverso = list(reversed(probe))
+                    id_cost = paths.index(reverso)  
+                SDMC_Cost[id_RS,id_RD,id_M,id_C] = path_cost[id_cost]
+                SDMC_Id_Medicao[id_RS,id_RD,id_M,id_C]=id_cost
+                id_C +=1
+        medicao_anterior = Medicao
+    pprint(SDMC_Cost) 
+    pprint(SDMC_Id_Medicao) 
+    End('Matriz de Custos')
 
-#for (i, item) in enumerate(Probes_List, start=0):
-   # print(i, item)
+    Start('Inicia o modelo e cria a função de maximização')
+    RS_list = [*range(0, total_roteadores,1)]
+    RD_list = [*range(0, total_roteadores,1)]
+    M_list = [*range(0,max(num_sonda_medicao) ,1)]
+    C_list = [*range(0, max_len_probe,1)]
+
+
+    SDMC_Dict = LpVariable.dicts("SDMC", (RS_list,RD_list,M_list,C_list), 0, 1, LpInteger)   
+
+    #pprint(SDMC_Dict)
+
+    modelo_colocacao = LpProblem("Probes Placement Model", LpMaximize)
+
+
+
+    modelo_colocacao += (lpSum([SDMC_Dict[i_RS][i_RD][0][0] * SDMC_Cost[i_RS][i_RD][0][0] for i_RS in RS_list for i_RD in RD_list for i_M in M_list for i_C in C_list if i_RS != i_RD]),"Total_Cost")
+    End('Inicia o modelo e cria a função de maximização')
+
+
+
+
+
+    Start('Limita sondas por roteador')
+
+    max_sondas = {n: (len(list(nx.all_neighbors(G, n))))for n in G.nodes}
+
+    #### ATENÇÃO VER ORIGEM DESTINO ######## 1-> 2 = 2 -> 1
+    for id_router, router in enumerate(routers):
+        #if id_router < (len(routers)-1):
+        pprint(f'Limite de sondas no roteador: {router} - {max_sondas.get(router)}')
+        #id_router = np.where(nodes_list == router)[0][0]
+        modelo_colocacao += (lpSum([SDMC_Dict[id_router][i_RD][0][0] for i_RD in RD_list if id_router != i_RD]) <=  max_sondas.get(router), "Max_Probes_Router" + str(id_router))
+
+    End('Limita sondas por roteador')
+
+    Start('Iguala as sondas')
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(IgualaSondas, paths)    
+    #IgualaSondas(paths)
+
+    End('Iguala as sondas')
+
+
+    #for i_RS in RS_list:
+    #    for i_RD in RD_list:
+    #        if i_RS != i_RD:
+    #            for i_M in M_list:
+    #                if SDMC_Cost[id_RS,id_RD,id_M,0] > 0:
+    #                    list_comp = []
+    #                    for i_C in C_list:
+    #                        if SDMC_Id_Medicao[i_RS][i_RD][i_M][i_C] > 0:
+    #                            list_comp.append(i_C)
+    #                    if len(list_comp) >0:
+    #                        modelo_colocacao += (lpSum([SDMC_Dict[i_RS][i_RD][i_M][i_C] for i_C in list_comp]) == len(list_comp), "Sonda" + str(i_RS) + str(i_RD) + str(i_M))
+
+    Start('Salva o Modelo')
+    modelo_colocacao.writeLP(rede.replace(".graphml", ".LP"))
+    End('Salva o Modelo')
+
+
+    Start('Pulp Solve')
+    modelo_colocacao.solve()
+    End('Pulp Solve')
+
+
+    print("Status:", LpStatus[modelo_colocacao.status])
+    if modelo_colocacao.status == 1:
+        #pprint(modelo_colocacao)    
+        for v in modelo_colocacao.variables():
+            if v.varValue > 0:
+                print(v.name, "=", v.varValue)
+                x = v.name.split("_")
+                #if (int(x[3]) == 0 ):
+                pprint(int(SDMC_Id_Medicao[int(x[1])][int(x[2])][int(x[3])][int(x[4])]))
+                pprint(paths[int(SDMC_Id_Medicao[int(x[1])][int(x[2])][int(x[3])][int(x[4])])])
+    print("Custo = ", value(modelo_colocacao.objective))
+
+
+    file = open('Probes_List.txt','w')
+    for itemmedicao in Probes_List:
+	    file.write(str(itemmedicao)+"\n")
+    file.close()
+
+    file2 = open('SDMC_Id_Medicao.txt','w')
+    for itemmedicao in SDMC_Id_Medicao:
+	    file2.write(str(itemmedicao)+"\n")
+    file2.close()
+
+
+    #pprint(SDMC_Id_Medicao)
+    #pprint(Probes_List)
+
+    #for (i, item) in enumerate(Probes_List, start=0):
+      # print(i, item)]
